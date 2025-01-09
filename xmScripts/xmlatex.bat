d1b6ead32b08c720fbd9e99f85201b4e812a44e4
xmlatex Xake <xmlatex@xakecontainer>
2025-01-09 11:35:27 -0500

    Added support for deploy

---
+++ xmScripts/xmlatex.bat
@@ -0,0 +1,7 @@
+@echo off
+set str=%*
+set "str=%str:\=/%"
+set "DOCKER_IMAGE=ghcr.io/ximeraproject/xake2024:v2.4.2"
+echo Starting docker %DOCKER_IMAGE% with args  %str%  in directory %cd%.
+echo docker run --rm --net="host" --mount type=bind,source=%cd%,target=/code %DOCKER_IMAGE% xmlatex %str%
+docker run --rm --net="host" --mount type=bind,source=%cd%,target=/code %DOCKER_IMAGE% xmlatex %str%
